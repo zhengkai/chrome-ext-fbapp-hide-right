4875fef4ce81c27c866cfedf921c0285e8705baa
zhengkai <zhengkai@gmail.com>
2016-02-26 13:13:30 +0800

    Initial commit

---
+++ background.js
@@ -0,0 +1,18 @@
+function listenPage(tabId, changeInfo, tab) {
+	if (changeInfo.status !== 'complete') {
+		return;
+	}
+	if (tab.status !== 'complete') {
+		return;
+	}
+
+	chrome.tabs.executeScript(tabId, {
+		file: "change.js"
+	}, function(result) {
+		if (chrome.runtime.lastError) {
+			return;
+		}
+	})
+}
+
+chrome.tabs.onUpdated.addListener(listenPage);
---
+++ change.js
@@ -0,0 +1,9 @@
+o = document.getElementById('pagelet_dock');
+if (o) {
+	o.remove();
+}
+
+o = document.getElementById('rightCol');
+if (o) {
+	o.remove();
+}
